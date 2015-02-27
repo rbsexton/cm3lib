d20ac07f2b87c47f171635250f2b12af4fe7fb07
rbsexton <robertbsexton@gmail.com>
2015-02-27 09:16:29 -0800

    A form that doesn't touch the NVIC.

--- userapp.s
+++ userapp.s
@@ -3,6 +3,10 @@
 @ system.  The user app is a Cortex-M executable that will
 @ take complete ownership of the system after the startup code
 @ does the hardware initialization.
+@
+@ In other contexts, this could be considered a normal part of 
+@ operating a bootloader.
+@ 
 @*************************************************************
 @ launchuserapp(uint32_t *addr) 
 @	R0: Starting address of the user app in memory.
@@ -29,4 +33,15 @@ launchuserapp:
 	pop { pc }
 		
 .end
-	
+
+.global LaunchUserAppNoNVIC
+.thumb
+.thumb_func
+
+LaunchUserAppNoNVIC:
+	ldr r1, [ r0, #0 ] /* Thats the stack pointer */
+	mov sp, r1
+	ldr r2, [ r0, #4 ] /* The initial PC */
+	push { r2 }
+	pop  { pc }
+.end
