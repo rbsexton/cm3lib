3f8150102ac4d7ca56aa66c664a883af737ce076
rbsexton <robertbsexton@gmail.com>
2017-06-14 22:51:37 -0700

    Clean up the names in the launcher code.

--- bl_launcher.h
+++ bl_launcher.h
@@ -1,5 +1,5 @@
 // C-Friendly Prototypes
+void LaunchUserApp(uint32_t *appaddr, uint32_t *runtimep); 
+void LaunchUserAppNoSP(uint32_t *appaddr, uint32_t *runtimep); 
 void LaunchUserAppUpdateNVIC(unsigned long *, unsigned long *runtime_p);  
-void LaunchUserAppNoNVIC(unsigned long *, unsigned long *runtime_p);  
-void LaunchUserAppNoSPNoNVIC(unsigned long *, unsigned long *runtime_p);  
 
