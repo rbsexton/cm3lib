19a0828e683531f66817bbfdef15abba640c8f0b
rbsexton <robertbsexton@gmail.com>
2017-10-29 19:02:59 -0700

    Documentation Cleanup.

--- ringbuffer.c
+++ ringbuffer.c
@@ -1,30 +1,33 @@
-// 
-// Lockless Ringbuffer routines
-// Copyright(C) 2012-2016 Robert Sexton
+/**
+@file ringbuffer.c
+@brief  Lockless Ringbuffer routines
+\copyright Copyright(C) 2012-2016 Robert Sexton
+@details 
+Basic Model - Struct with external storage
+- Lockless - No Irq protection required.
+- Dynamically created so that the same code maintains multiple buffers.
+- Counted contents so you can put in a big chunk.
+- Requires power of two sizing.
+
+Two pointers, reader and writer.  
+They both get used modulo the pointer size.   Equality means empty.
+- If the writer pre-empts the reader, it'll wrongly assume fullness.  No problem.
+- If the reader pre-empts the writer, it might wrongly assume empty.  No Problem.
+
+The read and write pointer are not allowed to wrap. 
+
+Return -1 rather than dropping characters on a failed add.
+The user must decide what to do in that case.
+*/
 //
-// Basic Model - Struct with external storage
-// - Lockless - No Irq protection required.
-// - Dynamically created so that the same code maintains multiple buffers.
-// - Counted contents so you can put in a big chunk.
-// - Requires power of two sizing.
-//
-// Ringbuffer, v2.   Make things more robust to better live in a world
-// without locking.   Two pointers, reader and writer.  
-// They both get used modulo the pointer size.   Equality means empty.
-// If the writer pre-empts the reader, it'll wrongly assume fullness.  No problem.
-// If the reader pre-empts the writer, it might wrongly assume empty.  No Problem.
-//  
-// Do not permit the read and write pointer to ever wrap.   Return -1 rather than
-// dropping characters.   The user must decide what to do in that case.
-// 
 
 #include <stdint.h>
 #include "ringbuffer.h"
 
-/// Initialization call.  
-/// @parameters
-/// a pointer to an ringbuffer structure
-/// a pointer to the buffer that will hold the data
+/// @brief Initialization call.  
+/// @param rb pointer to an ringbuffer structure
+/// @param buf pointer to the buffer that will hold the data
+/// @param size power of two size
 void ringbuffer_init(RINGBUF* rb, uint8_t* buf, int size) {
 	rb->iRead=0;   // Read index - points to the next character ready for reading
 	rb->iWrite=0;  // Write index - Points to next free character
@@ -34,26 +37,31 @@ void ringbuffer_init(RINGBUF* rb, uint8_t* buf, int size) {
 	rb->BufMask = size - 1;
 	}
 
-/// The number of characters 
+/// @brief The number of characters in the buffer
 // There used to be a not-empty check, but it turns out that
 // ringbuffer_used is a bit faster after compilation is over.
 // Used is just a subtract, so the conditional is free.	
+/// @return The number of bytes currently stored on the buffer
+/// @param rb pointer to a ringbuffer structure
 uint32_t ringbuffer_used(RINGBUF* rb) {
 	return(rb->iWrite - rb->iRead);
 	}
 
-/// The ring buffers are not allowed to fill up.
-/// That means that the maximal room is BufSize - 1 = BufMask 
+/// @brief How many characters before it fills up?
+/// @detail The ring buffers are not allowed to fill up.
+/// @detail That means that the maximal room is BufSize - 1 = BufMask 
+/// @param rb pointer to a ringbuffer structure
 uint32_t ringbuffer_free(RINGBUF* rb) {
 	return(rb->BufMask - ringbuffer_used(rb));
 	}
 
-/// Add a character to the ringbuffer, and return the 
-/// number of characters free in the ring buffer.
-/// 
-/// If incrementing the write counter makes it equal to 
-/// the read counter, that means that we're full.   Reject it and
-/// return -1 so that the producer can retry.
+/// @brief Add a character to the ringbuffer
+/// @return The number of remaining space in the ringbuffer, or -1 if full
+/// @param rb pointer to a ringbuffer structure
+/// @param c character to add to the ringbuffer
+// If incrementing the write counter makes it equal to 
+// the read counter, that means that we're full.   Reject it and
+// return -1 so that the producer can retry.
 int32_t ringbuffer_addchar(RINGBUF* rb, uint8_t c) {
 	uint32_t iWritePend = rb->iWrite + 1; 
 
@@ -69,8 +77,9 @@ int32_t ringbuffer_addchar(RINGBUF* rb, uint8_t c) {
 		}
 	}
 
-/// Pull a character out of the ringbuffer
-/// Return -1 if there is nothing in there. 
+/// @brief Pull a character out of the ringbuffer
+/// @return the next character in the ringbuffer, or -1 for empty. 
+/// @param rb pointer to a ringbuffer structure
 int ringbuffer_getchar(RINGBUF* rb) {
 	uint8_t c;
 	if ( ringbuffer_used(rb) ) { 
@@ -82,9 +91,9 @@ int ringbuffer_getchar(RINGBUF* rb) {
 	else return(-1);
 	}
 
-/// Reset the two pointers to prevent wrap.
-/// critial locking must come from outside.
-/// Ideally via a pended low priority interrupt.
+/// @brief Reset the two pointers to prevent wrap.
+/// @detail Not thread safe.
+/// @param rb pointer to a ringbuffer structure
 void ringbuffer_reset(RINGBUF* rb) {
 	if ( rb->iRead  < RB_COUNT_OVERFLOW ||
 	     rb->iWrite < RB_COUNT_OVERFLOW ) return;
@@ -94,16 +103,19 @@ void ringbuffer_reset(RINGBUF* rb) {
 	rb->ResetCount++;
 	}
 
-/// -----------------------------------------------------------
-/// Bulk operations.
-/// There are some conditions where we want to bypass the
-/// character-at-a-time aspect of things and do a bulk copy
-/// Networking and USB are the primary cases.
-/// So there will be one or two bulk operatons - one to collect
-/// all data up to the end of the storage area, and one to
-/// collect everything from the beginning.
-/// -----------------------------------------------------------
+// -----------------------------------------------------------
+// Bulk operations.
+// There are some conditions where we want to bypass the
+// character-at-a-time aspect of things and do a bulk copy
+// Networking and USB are the primary cases.
+// So there will be one or two bulk operatons - one to collect
+// all data up to the end of the storage area, and one to
+// collect everything from the beginning.
+// -----------------------------------------------------------
 
+/// @brief Get a pointer for a bulk removal.
+/// @return pointer ro the largest available contiguous block of characters.
+/// @param rb pointer to a ringbuffer structure
 // Pretty easy.   Just get the pointer to the next byte.
 uint8_t *ringbuffer_getbulkpointer(RINGBUF* rb) {
 	if ( ringbuffer_used(rb) ) { 
@@ -112,7 +124,8 @@ uint8_t *ringbuffer_getbulkpointer(RINGBUF* rb) {
 	else return(0);
 	}
 
-/// How many bytes are left?
+/// @return length of the largest available contiguous block of characters.
+/// @param rb pointer to a ringbuffer structure
 int32_t ringbuffer_getbulkcount(RINGBUF* rb) {
 	int used = ringbuffer_used(rb);
 	if ( used ) {
@@ -124,8 +137,10 @@ int32_t ringbuffer_getbulkcount(RINGBUF* rb) {
 		}
 	else return(used);
 	}
-	
-/// This one needs to run on trust.	
+
+/// @brief After a bulk read, we need to do a bulk remove	
+/// @param rb pointer to a ringbuffer structure
+/// @param count How many characters to remove.
 void ringbuffer_bulkremove(RINGBUF* rb, int count) {
 	rb->iRead += count;
 	}
